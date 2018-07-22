8553b25dbec6bb711c9f1997a7c6005bc489cfbc
Denis Mitrofanov <denis.mitr@gmail.com>
2018-07-22 12:11:21 +0300

    adds eloquent ORM

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,27 @@ $app = new \Jenssegers\Lean\App();
 $container = $app->getContainer();
 
 $container
-    ->add('settings')
-    ->set('displayErrorDetails', getenv('APP_DEBUG') === 'true')
-    ->set('name', getenv('APP_NAME'));
+    ->get('settings')
+    ->set('displayErrorDetails', getenv('APP_DEBUG') === 'true');
+
+$container->get('settings')->set('name', getenv('APP_NAME'));
+
+$container->get('settings')
+    ->set('db', [
+        'driver' => getenv('DB_DRIVER'),
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('DB_DATABASE'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'charset' => getenv('DB_CHARSET'),
+        'collation' => getenv('DB_COLLATION'),
+        'prefix' => ''
+    ]);
+
+
+$capsule = new \Illuminate\Database\Capsule\Manager();
+$capsule->addConnection($container->get('settings')->get('db'));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 require_once __DIR__ . '/../routes/web.php';
